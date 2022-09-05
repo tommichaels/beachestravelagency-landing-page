d91da4ede9b26ae2ad9c389e5d30d052804f0b66
Tomiwa Folorunso <78617644+tommichaels@users.noreply.github.com>
2022-09-05 13:47:43 +0100

    Imported assets, completed project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import React from 'react'
+import ImgCarousel from './components/carousel/ImgCarousel';
+import Destinations from './components/destinations/Destinations'
+import Footer from './components/footer/Footer';
+import Hero from './components/hero/Hero'
+import Navbar from './components/navbar/Navbar'
+import Search from './components/search/Search';
+import Selects from './components/selects/Selects';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <Navbar />
+      <Hero />
+      <Destinations />
+      <Search />
+      <Selects />
+      <ImgCarousel />
+      <Footer />
     </div>
   );
 }
